ce1dd3e93c5ff2f12cde33acd2a7d6360a4028b9
anudsa <133996511+anudsa@users.noreply.github.com>
2024-02-24 00:24:51 +0000

    tuples done

--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,16 @@
 # Tuples are immutable array-like structures
+coordinate = (2,3)
+x= coordinate[0]
+y= coordinate[1]
+print(f"The x axis is {x} & y is {y}, therefore the coordinate is {coordinate}")
+#function that returns 2 variables, area and perimeter of square
+def squareOperations(side):
+  area=side*side
+  perimeter=4*side
+  return (area,perimeter)
+
+area, perimeter = squareOperations(2)
+
+print(f"The area is: {area} The perimeter is: {perimeter}")
+
+
