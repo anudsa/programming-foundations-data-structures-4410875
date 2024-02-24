8dd80d58980091e1861aee3c2e430cf1bf1245d3
anudsa <133996511+anudsa@users.noreply.github.com>
2024-02-24 17:32:44 +0000

    challenge

--- 02/02_08/02_08b/main.py
+++ 02/02_08/02_08b/main.py
@@ -1,2 +1,8 @@
 my_list = [1, 7, 3]
-print(sorted(my_list))
+print(sorted(my_list, reverse = True))
+studentGrades=[("abc",23),("xyz",22),("poi",90),("qwe",32)]
+print(sorted(studentGrades))
+print(sorted(studentGrades,key= lambda inputTuple:inputTuple[0]))
+#Here we are sorting based on the element with index 1 of the tuple 
+print(sorted(studentGrades,key= lambda inputTuple:inputTuple[1]))
+print(sorted(studentGrades,key= lambda inputTuple:inputTuple[1],reverse=True))
--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,15 @@
-def find_second_smallest(my_list):
-    return 0
+#Create a function that finds the second smallest item in a list of integers
+#The list is not sorted
+#The list could be empty or only have one item, if so, return none.
+#No duplicates in the list
+list1 = [5, 8, 3, 2, 6]
+list2 = []
+list3 = [1]
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+def find_second_smallest(my_list):
+    if (len(my_list)) < 2:
+        return "none"
+    else:
+        return sorted(my_list)[1]
+
+print(find_second_smallest(list3))
